0388406becae182c9acdd898c245d363012e27a9
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-02-05 18:37:22 +0000

    style brand

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar mb-3">
-			<Link to="/">
+			<Link to="/" className="LinkBrand">
 				<span className="navbar-brand mb-0 h1">Contact List</span>
 			</Link>
 		</nav>
